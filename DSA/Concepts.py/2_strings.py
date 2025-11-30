f600d4e1e38847d8ff12d283bac3591db040ab43
Ameya Kulkarni <acclaptop47@gmail.com>
2025-11-30 16:54:57 +0530

    Delete DSA directory

--- DSA/Concepts.py/2_strings.py
+++
@@ -1,4 +0,0 @@
-#strings are immutable i n python in c they are mutable
-#cannot append
-#as modification is not possible mostly every operation is o(n)
-#apart from indexing and random access
--- DSA/questions/10_find diff.py
+++
@@ -1,8 +0,0 @@
-from collections import Counter
-class Solution:
-    def findTheDifference(self, s: str, t: str) -> str:
-        count_s = Counter(s) # {a:1,b:1,c:1,d:1}
-        count_t = Counter(t) # {a:1,b:1,c:1,d:1,e:1}
-        for i in count_t: # i will iterate through a , b ...
-            if i not in count_s or count_s[i] < count_t[i]:
-                return i
